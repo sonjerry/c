212572c992fb48ed5a3a3a0de6024193c354056c
sonjerry <101078893+sonjerry@users.noreply.github.com>
2022-03-22 13:07:08 +0900

    update

--- polynomial_1.c
+++ polynomial_1.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-
 #define MAX(a,b) (((a)>(b))?(a):(b))
-
-#define MAX_DEGREE 101
+#define MAX_DEGREE 10
 
 typedef struct { // 다항식 구조체 타입 선언
 
 int degree; // 다항식의 차수
-
 float coef[MAX_DEGREE]; // 다항식의 계수
 
 } polynomial;
@@ -23,9 +20,7 @@ polynomial poly_add1(polynomial A, polynomial B)
 polynomial C; // 결과 다항식
 
 int Apos = 0, Bpos = 0, Cpos = 0; // 배열 인덱스 변수
-
 int degree_a = A.degree;
-
 int degree_b = B.degree;
 
 C.degree = MAX(A.degree, B.degree); // 결과 다항식 차수
@@ -47,8 +42,6 @@ else if (degree_a == degree_b) {  // A항 == B항
 C.coef[Cpos++] = A.coef[Apos++] + B.coef[Bpos++];
 
 degree_a--; degree_b--;
-
- 
 }
 
 else { // B항 > A항
@@ -58,15 +51,10 @@ C.coef[Cpos++] = B.coef[Bpos++];
 degree_b--;
 
 }
-
 }
 
 return C;
-
 }
-
- 
-
 void print_poly(polynomial p)
 
 {
@@ -79,29 +67,36 @@ printf("%3.1f \n", p.coef[p.degree]);
 
 }
 
- 
-
-// 주함수
+//=====================================================
 
 int main(void)
 
 {
+int a_degree = 0;
+int b_degree = 0;
+float arr_a[MAX_DEGREE];
+float arr_b[MAX_DEGREE];
+printf("첫 번째 다항식:");
+for (int i=0;i<MAX_DEGREE+1;i++){
+    scanf("%f",&arr_a[i]);
+}
+a_degree = arr_a[0];
 
-polynomial a = { 5,{ 3, 6, 0, 0, 0, 10 } };
+printf("두 번째 다항식:");
+for (int i = 0;i<MAX_DEGREE+1;i++){
+    scanf("%f",&arr_b[i]);
+}
+b_degree = arr_b[0];
 
-polynomial b = { 4,{ 7, 0, 5, 0, 1 } };
+polynomial a = { a_degree, *arr_a };
+polynomial b = { b_degree, *arr_b };
 
 polynomial c;
 
- 
-
 print_poly(a);
-
 print_poly(b);
-
 c = poly_add1(a, b);
 
- 
 printf("-----------------------------------------------------------------------------\n");
 
 print_poly(c);
