ef069d348b8399efa043ac534af2398795e4d252
sonjerry <101078893+sonjerry@users.noreply.github.com>
2022-03-29 14:21:33 +0900

    commit

--- arr.c
+++ arr.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 #define MAX_TERMS 100
+#define MAX(a,b) (((a)>(b))?(a):(b))
 
 typedef struct {
 
 int row;
-
 int col;
-
 int value;
 
 } element;
@@ -17,11 +16,8 @@ int value;
 typedef struct SparseMatrix {
 
 element data[MAX_TERMS];
-
 int rows; // 행의 개수
-
 int cols; // 열의 개수
-
 int terms; // 항의 개수
 
 } SparseMatrix;
@@ -30,23 +26,65 @@ int terms; // 항의 개수
 
  
 SparseMatrix etos(int arr[8][10]) {
+
     SparseMatrix b;
     int bindex = 0;
-
+    b.terms = 0;
     b.rows = 8;
     b.cols = 10;
 
-    for(int i=0; i<strlen(arr); i++){
-    for(int j=0; j<strlen(arr[i]); j++){
+    for(int i=0; i<7; i++){
+    for(int j=0; j<9; j++){
         if(arr[i][j] != 0){
             b.data[bindex].row = i;
             b.data[bindex].col = j;
             b.data[bindex].value = arr[i][j];
+
             bindex++;
+            b.terms++;
         }
+    }
     }
     return b;
 }
+SparseMatrix plus(SparseMatrix a, SparseMatrix b){
+    
+    SparseMatrix c;
+    int rows = MAX(a.terms,b.terms);
+    int cols = MAX(a.cols,b.cols);
+
+
+    for(int i=0; i<rows; i++){
+    for(int j=0; j<cols; j++){
+
+    if (a.data[i].row == b.data[i].row && a.data[i].col == b.data[i].col){ //두 행,열이 겹칠 경우 더함
+
+            c.data[i].row = a.data[i].row;
+            c.data[i].col = a.data[i].col;
+            c.data[i].value = a.data[i].value + b.data[i].value;
+        
+    }
+
+    }
+    }
+
+   
+    return c;
+}
+
+void matrix_print(SparseMatrix a){
+    printf("====================\n");
+    for(int i=0; i<a.terms;i++){
+        printf("(%d, %d, %d) \n", a.data[i].row, a.data[i].col, a.data[i].value);
+    }
+    printf("====================\n");
+}
+
+void matrix_print2(SparseMatrix a){
+    printf("====================\n");
+
+}
+
 
 int main() {
 
@@ -102,6 +140,13 @@ int main() {
 
                              { 0, 0, 0, 0, 0, 0, 4, 0, 0, 0 }};
 
-
+    SparseMatrix new;
+    new = etos(a);
+    matrix_print(new);
+    new = etos(b);
+    matrix_print(new);
+    new = etos(c);
+    matrix_print(new);
+    return 0;
 
 }
